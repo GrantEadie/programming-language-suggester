2d3ebeb932ffed5ce7876513023aa5e247714697
Grant Eadie <grantleadie@gmail.com>
2020-08-14 10:38:39 -0700

    Adds last question.

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@
 $(document).ready(function() {
   $("form#coding").submit(function(event) {
     event.preventDefault();
+    const difficulty = ("#difficulty").val();
     const webDev = $("#webDev").val();
     const hardwareDev = $("#hardwareDev").val();
     const dataSci = $("#dataSci").val();
@@ -12,15 +13,19 @@ $(document).ready(function() {
 
       // y/y/y/y
     if (webDev === "yes" && hardwareDev === "yes" && dataSci === "yes" && apple === "yes") {
-      alert("Python is great for the general stuff, but you'll have to learn Swift to work with Apple");
+      alert("Python is great for the general stuff, but you'll have to learn Swift to work with Apple.");
 
       //  y/y/y/n
     } else if (webDev === "yes" && hardwareDev === "yes" && dataSci === "yes" && apple === "no") {
-      alert("Python is great for general work in the industry");
+      alert("Python is great for general work in the industry.");
 
-       //  y/y/y/n
+       //  y/y/n/n
     } else if (webDev === "yes" && hardwareDev === "yes" && dataSci === "no" && apple === "no") {
-      alert("Python is great for overall work, but you'll likely want to boost your web skills with ");
+      alert("Python is great for overall work, but you'll likely want to boost your web skills with Ruby or Javascript.");
+
+       //  y/n/n/n
+    } else if (webDev === "yes" && hardwareDev === "no" && dataSci === "no" && apple === "no") {
+      alert("Javascript will be your best bet to start off with. Ruby is the next logical step, followed by Go.");
 
       // y/y/n/y
     } else if (webDev === "yes" && hardwareDev === "yes" && dataSci === "no" && apple === "yes") {
