e73c3b738531f7c3683f535b5d9d03b9cc86d78a
Grant Eadie <grantleadie@gmail.com>
2020-08-14 10:22:37 -0700

    Adds two more questions.

--- js/scripts.js
+++ js/scripts.js
@@ -7,16 +7,36 @@ $(document).ready(function() {
     event.preventDefault();
     const webDev = $("#webDev").val();
     const hardwareDev = $("#hardwareDev").val();
+    const dataSci = $("#dataSci").val();
+    const apple = $("apple").val();
 
-  
-    if (webDev === "yes" && hardwareDev === "yes") {
-      alert("yes");
+      // y/y/y/y
+    if (webDev === "yes" && hardwareDev === "yes" && dataSci === "yes" && apple === "yes") {
+      alert("Python is great for the general stuff, but you'll have to learn Swift to work with Apple");
 
-    } else if (webDev === "yes" && hardwareDev === "no") {
-      alert("maybe");
-      
+      //  y/y/y/n
+    } else if (webDev === "yes" && hardwareDev === "yes" && dataSci === "yes" && apple === "no") {
+      alert("Python is great for general work in the industry");
+
+       //  y/y/y/n
+    } else if (webDev === "yes" && hardwareDev === "yes" && dataSci === "no" && apple === "no") {
+      alert("Python is great for overall work, but you'll likely want to boost your web skills with ");
+
+      // y/y/n/y
+    } else if (webDev === "yes" && hardwareDev === "yes" && dataSci === "no" && apple === "yes") {
+      alert("You'll have to learn Swift for Apple, but consider Python, Go, or Javascript as well");
+
+    } else if (webDev === "yes" && hardwareDev === "no" && dataSci === "no") {
+      alert("Ruby, Javascript, or Go");
+    
+    } else if (webDev === "no" && hardwareDev === "no" && dataSci === "yes") {
+      alert("C#, or Python");
+
+    } else if (webDev === "no" && hardwareDev === "yes" && dataSci === "no") {
+      alert("C#, or Python");
+
     } else {
-        alert("no");
+        alert("You'll have to learn two programs");
       }
   });
 });
