9b06fe7382187cc85ff05fdbb2c9fe36329571ef
Grant Eadie <grantleadie@gmail.com>
2020-08-14 09:37:00 -0700

    adds another question.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,21 @@
 
 
 
-// user interface
+// User interface for Coding.
 $(document).ready(function() {
   $("form#coding").submit(function(event) {
     event.preventDefault();
     const webDev = $("#webDev").val();
+    const hardwareDev = $("#hardwareDev").val();
 
   
-    if (webDev === "yes") {
+    if (webDev === "yes" && hardwareDev === "yes") {
       alert("yes");
-      } else {
+
+    } else if (webDev === "yes" && hardwareDev === "no") {
+      alert("maybe");
+      
+    } else {
         alert("no");
       }
   });
