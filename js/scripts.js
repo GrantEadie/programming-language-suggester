bcb5572c4b32e0dcfe97880eccaffa0fe86a0b0a
Grant Eadie <grantleadie@gmail.com>
2020-08-14 09:13:21 -0700

    change previous js concepts but will not function.

--- js/scripts.js
+++ js/scripts.js
@@ -5,39 +5,34 @@
 $(document).ready(function() {
   $("form#profileInfo").submit(function(event) {
     event.preventDefault();
-    let age = $("#age").val();
+    let web = $("#webDev").val();
     let color = $("#favColor").val();
     let wyr = $("input:radio[name=wyr]:checked").val();
 
     $(".skeleton, .barney, .batman, .edward, .karen").hide();
 
-
-    if (age) {   
-      if (age < 18) {
-        alert("You're to young, go back to school!");
-        }
-        else if (age > 65) {
-          $(".skeleton, .matchTitle").show();    
-        }
-        else if (age >= 18 && age < 25 || wyr === "jumping") {
-          $(".barney, .matchTitle").show();
-        }  
-        else if (color === "Black" && wyr === "savingPeople") {
-          $(".batman, .matchTitle").show();
-        } 
-        else if (color === "Black" && wyr === "gardening") {
-          $(".edward, .matchTitle").show();
-        }
-        else if (color === "white" || wyr === "complaining" ) {
-          $(".karen, .matchTitle").show(); 
-        }        
-        else {
-          $(".batman, .matchTitle").show();
-        }      
+  
+    if (web === "yes") {
+      alert("You're to young, go back to school!");
       }
+      else if (age > 65) {
+        $(".skeleton, .matchTitle").show();    
+      }
+      else if (age >= 18 && age < 25 || wyr === "jumping") {
+        $(".barney, .matchTitle").show();
+      }  
+      else if (color === "Black" && wyr === "savingPeople") {
+        $(".batman, .matchTitle").show();
+      } 
+      else if (color === "Black" && wyr === "gardening") {
+        $(".edward, .matchTitle").show();
+      }
+      else if (color === "white" || wyr === "complaining" ) {
+        $(".karen, .matchTitle").show(); 
+      }        
       else {
-        alert("Please enter your age");
-      }
+        $(".batman, .matchTitle").show();
+      }      
   });
 });
 
