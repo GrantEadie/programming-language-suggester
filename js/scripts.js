803c39c2cb8ead851e75b16fb866dacd6ae3583d
Grant Eadie <grantleadie@gmail.com>
2020-08-14 12:33:33 -0700

    add css touches with bootstrap implementaton.

--- js/scripts.js
+++ js/scripts.js
@@ -68,92 +68,92 @@ $(document).ready(function() {
 
       //  n/n/y/y/n
     } else if (webDev === "no" && hardwareDev === "no" && dataSci === "yes" && apple === "yes" && dif === "no") {
-      alert("Python and Swift for you my friend.");
+      $(".matchTitle").text("Python and Swift for you my friend.");
       $(".match").show();
 
       //  n/n/y/n/n
     } else if (webDev === "no" && hardwareDev === "no" && dataSci === "yes" && apple === "no" && dif === "no") {
-      alert("Python. Without a doubt.");
+      $(".matchTitle").text("Python. Without a doubt.");
       $(".match").show();
 
       //  n/n/n/y/y
     } else if (webDev === "no" && hardwareDev === "no" && dataSci === "no" && apple === "yes" && dif === "yes") {
-      alert("You'll likely only need Swift. But consider Python and C# to give you a wider understanding.");
+      $(".matchTitle").text("You'll likely only need Swift. But consider Python and C# to give you a wider understanding.");
       $(".match").show();
 
       //  n/n/n/y/n
     } else if (webDev === "no" && hardwareDev === "no" && dataSci === "no" && apple === "yes" && dif === "no") {
-      alert("Swift, that's all you need for now.");
+      $(".matchTitle").text("Swift, that's all you need for now.");
       $(".match").show();
           
       //  n/n/n/n/y
     } else if (webDev === "no" && hardwareDev === "no" && dataSci === "no" && apple === "no" && dif === "yes") {
-      alert("If you have no intention of working in the industry, could be work learning Go and Rust for their interesting properties.");
+      $(".matchTitle").text("If you have no intention of working in the industry, could be work learning Go and Rust for their interesting properties.");
       $(".match").show();
     
       //  y/n/n/n/y
     } else if (webDev === "yes" && hardwareDev === "no" && dataSci === "no" && apple === "no" && dif === "yes") {
-      alert("Ruby and Java are for you.");
+      $(".matchTitle").text("Ruby and Java are for you.");
       $(".match").show();
       
       //  y/n/n/y/y
     } else if (webDev === "yes" && hardwareDev === "no" && dataSci === "no" && apple === "yes" && dif === "yes") {
-      alert("Ruby and Java for web development and Swift for iOS.");
+      $(".matchTitle").text("Ruby and Java for web development and Swift for iOS.");
       $(".match").show();
     
       //  y/n/y/y/y
     } else if (webDev === "yes" && hardwareDev === "no" && dataSci === "yes" && apple === "yes" && dif === "yes") {
-      alert("Python and Swift. Get to it.");
+      $(".matchTitle").text("Python and Swift. Get to it.");
       $(".match").show();
 
       //  n/y/n/n/y
     } else if (webDev === "no" && hardwareDev === "yes" && dataSci === "no" && apple === "no" && dif === "yes") {
-      alert("C# is the best for you.");
+      $(".matchTitle").text("C# is the best for you.");
       $(".match").show();
 
       //  n/y/n/y/y
     } else if (webDev === "no" && hardwareDev === "yes" && dataSci === "no" && apple === "yes" && dif === "yes") {
-      alert("C# for hardware development and Swift for Apple stuff.");
+      $(".matchTitle").text("C# for hardware development and Swift for Apple stuff.");
       $(".match").show();
 
       //  y/y/n/y/y
     } else if (webDev === "yes" && hardwareDev === "yes" && dataSci === "no" && apple === "yes" && dif === "yes") {
-      alert("Start with Javascript, move to C# and finish up with Ruby. You'll eventually want Python for the versitility and Swift for Apple.");
+      $(".matchTitle").text("Start with Javascript, move to C# and finish up with Ruby. You'll eventually want Python for the versitility and Swift for Apple.");
       $(".match").show();
 
       //  y/n/y/y/n
     } else if (webDev === "yes" && hardwareDev === "no" && dataSci === "yes" && apple === "yes" && dif === "no") {
-      alert("You'll absolutely need Python, but consider getting into Ruby for a better understanding of web development.");
+      $(".matchTitle").text("You'll absolutely need Python, but consider getting into Ruby for a better understanding of web development.");
       $(".match").show();
 
       //  y/y/y/n/y
     } else if (webDev === "yes" && hardwareDev === "yes" && dataSci === "yes" && apple === "no" && dif === "yes") {
-      alert("Javascript first, C#, Ruby, then Python. You got this!");
+      $(".matchTitle").text("Javascript first, C#, Ruby, then Python. You got this!");
       $(".match").show();
 
       //  y/n/n/y/n
     } else if (webDev === "yes" && hardwareDev === "no" && dataSci === "no" && apple === "yes" && dif === "no") {
-      alert("Javascript and Ruby first, then make the jump to Swift to work with Apple.");
+      $(".matchTitle").text("Javascript and Ruby first, then make the jump to Swift to work with Apple.");
       $(".match").show();
 
       //  y/n/y/n/n
     } else if (webDev === "yes" && hardwareDev === "no" && dataSci === "yes" && apple === "no" && dif === "no") {
-      alert("Start with Javascript and move to Python. Consider Ruby to boost your web development skills.");
+      $(".matchTitle").text("Start with Javascript and move to Python. Consider Ruby to boost your web development skills.");
       $(".match").show();
 
       //  n/y/n/y/n
     } else if (webDev === "no" && hardwareDev === "yes" && dataSci === "no" && apple === "yes" && dif === "no") {
-      alert("It won't be easy, but you'll need C# if you want to get into hardware development, but that'll give you a good understanding for Swift (Apple's language). Sorry kiddo.");
+      $(".matchTitle").text("It won't be easy, but you'll need C# if you want to get into hardware development, but that'll give you a good understanding for Swift (Apple's language). Sorry kiddo.");
       $(".match").show();
 
       //  y/y/n/n/y
     } else if (webDev === "yes" && hardwareDev === "yes" && dataSci === "no" && apple === "no" && dif === "yes") {
-      alert("Start with Javascript, move to C# then Ruby and Python. You got this!");
+      $(".matchTitle").text("Start with Javascript, move to C# then Ruby and Python. You got this!");
       $(".match").show();
 
       //  n/y/n/y/n
     } else if (webDev === "no" && hardwareDev === "yes" && dataSci === "no" && apple === "yes" && dif === "no") {
-      alert("It won't be easy, but you'll need C# if you want to get into hardware development, but that'll give you a good understanding for Swift (Apple's language). Sorry kiddo.");
+      $(".matchTitle").text("It won't be easy, but you'll need C# if you want to get into hardware development, but that'll give you a good understanding for Swift (Apple's language). Sorry kiddo.");
       $(".match").show();
 
     } else {
